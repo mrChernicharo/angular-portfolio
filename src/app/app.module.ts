b77892e0a08ad1159af82505c427c1e405b0ca66
Felipe Chernicharo <string7dev@gmail.com>
2020-11-23 10:40:57 -0300

    iniciando liveExample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,19 +10,42 @@ import { AboutComponent } from "./pages/about/about.component";
 import { ProjectsComponent } from "./pages/projects/projects.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
-import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSelectModule } from "@angular/material/select";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
 import { MatTabsModule } from "@angular/material/tabs";
-import { NavComponent } from './components/nav/nav.component';
-import { LiveExamplesComponent } from './pages/live-examples/live-examples.component';
-import { BlogComponent } from './pages/blog/blog.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+
+// import {} from '@angular/material/'
+// import {} from '@angular/material'
+
+import { NavComponent } from "./components/nav/nav.component";
+import { LiveExamplesComponent } from "./pages/live-examples/live-examples.component";
+import { BlogComponent } from "./pages/blog/blog.component";
+
+const matModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatIconModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatSelectModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+];
 
 @NgModule({
   declarations: [
@@ -38,16 +62,9 @@ import { BlogComponent } from './pages/blog/blog.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatDialogModule,
-    MatListModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatSnackBarModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...matModules,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 
--- src/app/pages/live-examples/live-examples.component.ts
+++ src/app/pages/live-examples/live-examples.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { BehaviorSubject, from, Observable, of } from "rxjs";
+import { debounceTime, map, tap, throttleTime } from "rxjs/operators";
 
 @Component({
-  selector: 'app-live-examples',
-  templateUrl: './live-examples.component.html',
-  styleUrls: ['./live-examples.component.scss']
+  selector: "app-live-examples",
+  templateUrl: "./live-examples.component.html",
+  styleUrls: ["./live-examples.component.scss"],
 })
 export class LiveExamplesComponent implements OnInit {
+  tempo = 100;
+  timeSignature = 4;
+  isPlaying = false;
+  timeSignature$ = new BehaviorSubject(4);
+  beatLength$: Observable<any[]>;
+  form: FormGroup;
+  instruments = ["hi-hat", "snare", "bass-kick"];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.timeSignature$.pipe(debounceTime(300)).subscribe((time) => {
+      const arr = [];
+      arr.length = time;
+      this.beatLength$ = of(arr);
+    });
+
+    this.form = this.fb.group({
+      tempo: [100, Validators.max(300)],
+      timeSignature: [4],
+    });
+
+    this.form.valueChanges.pipe(debounceTime(300)).subscribe((formValue) => {
+      console.log(formValue);
+      this.timeSignature$.next(formValue["timeSignature"]);
+    });
+
+    // this.beatLength$.pipe(
+    //   map((d) => (d.length = this.timeSignature * 4)),
+    //   tap((d) => console.log(d))
+    // );
   }
 
+  onSubmit() {}
+
+  handleLoop() {
+    this.isPlaying = !this.isPlaying;
+  }
+
+  animate() {
+    document.querySelector("");
+  }
 }
