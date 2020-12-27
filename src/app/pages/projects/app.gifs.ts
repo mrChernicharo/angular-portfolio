c598f3b62d2949cb46fa5e1b740ce8994a7171ec
Felipe Chernicharo <string7dev@gmail.com>
2020-12-27 18:16:04 -0300

    renderizando gifs na página de projetos

---
+++ src/app/pages/projects/app.gifs.ts
@@ -0,0 +1,9 @@
+export const appGifs = {
+  gif0: "../../../assets/gifs/mosquito_killer_ok.gif",
+  gif1: "../../../assets/gifs/app-lista-tarefas.gif",
+  gif2: "../../../assets/gifs/car_rush_ok.gif",
+  gif3: "../../../assets/gifs/go-marketplace.gif",
+  gif4: "../../../assets/gifs/i-recipe.gif",
+  gif5: "../../../assets/gifs/angular_crud.gif",
+  gif6: "../../../assets/gifs/ecoleta.gif",
+};
